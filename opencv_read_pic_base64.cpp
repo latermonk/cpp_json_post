a8ae0e8fbca0ec09818e91c62492c5e2fea0be4c
1405984387 <1405984387@qq.com>
2019-06-18 13:27:24 +0800

    Create opencv_read_pic_base64.cpp

---
+++ opencv_read_pic_base64.cpp
@@ -0,0 +1,89 @@
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <chrono>
+#include <thread>
+#include <io.h>
+
+using namespace std;
+using namespace cv;
+
+
+
+static std::string base64Encode(const unsigned char* Data, int DataByte)
+{
+	//编码表
+	const char EncodeTable[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	//返回值
+	std::string strEncode;
+	unsigned char Tmp[4] = { 0 };
+	int LineLength = 0;
+	for (int i = 0; i < (int)(DataByte / 3); i++)
+	{
+		Tmp[1] = *Data++;
+		Tmp[2] = *Data++;
+		Tmp[3] = *Data++;
+		strEncode += EncodeTable[Tmp[1] >> 2];
+		strEncode += EncodeTable[((Tmp[1] << 4) | (Tmp[2] >> 4)) & 0x3F];
+		strEncode += EncodeTable[((Tmp[2] << 2) | (Tmp[3] >> 6)) & 0x3F];
+		strEncode += EncodeTable[Tmp[3] & 0x3F];
+		if (LineLength += 4, LineLength == 76) { strEncode += "\r\n"; LineLength = 0; }
+	}
+	//对剩余数据进行编码
+	int Mod = DataByte % 3;
+	if (Mod == 1)
+	{
+		Tmp[1] = *Data++;
+		strEncode += EncodeTable[(Tmp[1] & 0xFC) >> 2];
+		strEncode += EncodeTable[((Tmp[1] & 0x03) << 4)];
+		strEncode += "==";
+	}
+	else if (Mod == 2)
+	{
+		Tmp[1] = *Data++;
+		Tmp[2] = *Data++;
+		strEncode += EncodeTable[(Tmp[1] & 0xFC) >> 2];
+		strEncode += EncodeTable[((Tmp[1] & 0x03) << 4) | ((Tmp[2] & 0xF0) >> 4)];
+		strEncode += EncodeTable[((Tmp[2] & 0x0F) << 2)];
+		strEncode += "=";
+	}
+
+
+	return strEncode;
+}
+
+
+
+
+
+
+static std::string Mat2Base64(const cv::Mat &img, std::string imgType)
+{
+	//Mat转base64
+	std::string img_data;
+	std::vector<uchar> vecImg;
+	std::vector<int> vecCompression_params;
+	vecCompression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
+	vecCompression_params.push_back(100);
+	imgType = "." + imgType;
+	cv::imencode(imgType, img, vecImg, vecCompression_params);
+	img_data = base64Encode(vecImg.data(), vecImg.size());
+	return img_data;
+}
+
+
+
+int main(){
+    Mat img = imread("./face.jpg");
+    string  base64_code = Mat2Base64(img, "jpg");
+    cout << "===imgbase64==== ===imgbase64=== >> " << base64_code << endl;
+
+
+}
+
+
+
+
